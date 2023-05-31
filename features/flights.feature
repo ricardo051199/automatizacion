5cd2f5a4b92b6f62afa47ba6e783a50494d6350f
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-05-31 17:38:36 -0600

    First automated tests - flights

--- features/flights.feature
+++ features/flights.feature
@@ -2,18 +2,45 @@ Feature: As a internet user
          I want to use the google search engine
          so I test that works correctly
 
-Scenario: Search a flight with no seats avaialble in Mercury Tours page        
-  Given I am browsing the "https://demo.guru99.com/test/newtours/index.php" page.
+Background:
+  Given I am on the Mercury Tours homepage
   When I will click the "Flights" link 
-  And I enter the required fields as show below
-    |Type:                   |                    |
+
+Scenario: Search for a flight with available seats in Mercury Tours page.      
+  Given I enter the required fields as show below
+    |Type:                   | Round Trip         |
     |Passengers:             | 2                  |
     |Departing From:         | Acapulco           |
     |On:                     | January-1          |
     |Arriving In:            | Zurich             |  
     |Returning:              | February-1         |
-    |Service Class:          |                    |
-    |Airline:                | Pangea Airlines    |
-  And send my flight search form
-  Then I see text above after flight search engine "After flight finder - No Seats Avaialble  "
+    |Service Class:          | Business class     |
+    |Airline:                | No Preference      |
+  When send my flight search form
+  Then I see text above after flight search engine "After flight finder - Seats Avaialble"
   
+Scenario: Search a flight with no seats avaialble in Mercury Tours page.     
+  Given I enter the required fields as show below
+    |Type:                   | One Way            |
+    |Passengers:             | 4                  |
+    |Departing From:         | London             |
+    |On:                     | August-1           |
+    |Arriving In:            | Paris              |  
+    |Returning:              | September-1        |
+    |Service Class:          | First class        |
+    |Airline:                | Blue Skies Airlines|
+  When send my flight search form
+  Then I see text above after flight search engine "After flight finder - No Seats Avaialble  "
+
+Scenario: Search for a flight with the same origin and destination in Mercury Tours page.       
+  Given I enter the required fields as show below
+    |Type:                   | One Way            |
+    |Passengers:             | 1                  |
+    |Departing From:         | New York           |
+    |On:                     | November-1         |
+    |Arriving In:            | New York           |  
+    |Returning:              | December-1         |
+    |Service Class:          | Economy class      |
+    |Airline:                | No Preference      |
+  When send my flight search form
+  Then I see an error message stating that "The source and destination must be different"
--- features/step_definitions/flightsSteps.rb
+++ features/step_definitions/flightsSteps.rb
@@ -1,7 +1,7 @@
-4#Given I am on the Mercury Tours page
-Given(/^I am browsing the "([^"]*)" page.$/) do |mercuryToursPage|
+#Given I am on the Mercury Tours page
+Given(/^I am on the Mercury Tours homepage$/) do
   page.driver.browser.manage.window.maximize
-  visit (mercuryToursPage)
+  visit('http://demo.guru99.com/test/newtours/')
 end
 
 #Then I will click the "Flights" link
@@ -9,33 +9,44 @@ Then(/^I will click the "([^"]*)" link$/) do |flightsLink|
   click_link(flightsLink)
 end
 
-
 When(/^I enter the required fields as show below$/) do |table|
   data = table.rows_hash
   data.each_pair do |key, value|
     case key
   when "Type:"
-    xpath_base = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]'
-    find(:xpath, xpath_base).click
+    case value
+    when "Round Trip"
+      search_selector  = "input[value='roundtrip']"
+    when "One Way"
+      search_selector = "input[value='oneway']"
+    end
+    find(search_selector).click
   when "Passengers:"
     select(value, :from => 'passCount')
   when "Departing From:"
     select(value, :from => 'fromPort')
   when "On:"
-    mes = /[a-zA-Z]*/.match(value)
+    month = /[a-zA-Z]*/.match(value)
     day = /\d/.match(value)
-    select(mes, :from => 'fromMonth')
-    select(day, :from => 'fromDay')
+    all('select')[2].find('option', text: month).select_option
+    all('select')[3].find('option', text: day, match: :first).select_option
   when "Arriving In:"
     select(value, :from => 'toPort')
   when "Returning:"
-    mes = /[a-zA-Z]*/.match(value)
+    month = /[a-zA-Z]*/.match(value)
     day = /\d/.match(value)
-    select(mes, :from => 'fromMonth')
-    select(day, :from => 'fromDay')
+    all('select')[5].find('option', text: month).select_option
+    all('select')[6].find('option', text: day, match: :first).select_option
   when "Service Class:"
-    xpath_base = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[2]'
-    find(:xpath, xpath_base).click
+    case value
+    when "Economy class"
+      search_selector  = "input[value='Coach']"
+    when "Business class"
+      search_selector = "input[value='Business']"
+    when "First class"
+      search_selector = "input[value='First']"
+    end
+    find(search_selector).click
   when "Airline:"
     select(value, :from => 'airline')
   end
@@ -43,15 +54,19 @@ When(/^I enter the required fields as show below$/) do |table|
 end
 
 When(/^send my flight search form$/) do
-  xpath_base = '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td/input'
-  find(:xpath, xpath_base).click
+  find("input[name='findFlights']").click
 end
 
 #I see text above after flight search engine "After flight finder - No Seats Avaialble  "
 Then('I see text above after flight search engine {string}') do |flightFinder|
-  flightFinderLabel = find(:xpath, '/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td/p/font/b/font[1]')
+  flightFinderLabel = all('font')[4]
   puts "ONLY FOR TEST PURPOSES:"+flightFinderLabel.text
   if flightFinderLabel.text != flightFinder
       raise "Flight search should be"+flightFinder 
   end
+end
+
+#I see an error message stating that "The source and destination must be different"
+Then('I see an error message stating that {string}') do |errorMessage|
+  puts errorMessage
 end
